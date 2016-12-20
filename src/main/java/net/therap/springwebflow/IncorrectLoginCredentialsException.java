2b0e75e0dd11625254560ef666e049e53676795c
Rumman Bin Ashraf <rumman@therapservices.net>
2016-12-20 15:52:47 +0600

    Fix

---
+++ src/main/java/net/therap/springwebflow/IncorrectLoginCredentialsException.java
@@ -0,0 +1,11 @@
+package net.therap.springwebflow;
+
+/**
+ * @author rumman
+ * @since 12/20/16
+ */
+public class IncorrectLoginCredentialsException extends Exception{
+
+    private static final long serialVersionUID = 3339021692474487380L;
+
+}
